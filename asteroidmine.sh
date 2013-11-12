eba54966f5930feeca71c62affba42ade397c319
Alexey Komissarouk <alexey@alexeymk.com>
2013-11-11 21:19:56 -0500

    theoreetically working version. Next step: test once I have internet

--- asteroidmine.sh
+++ asteroidmine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Download the user's prod db into a local meteor db for testing/etc.
 
+USAGE="Usage: asteroidmine.sh YOURAPP.meteor.com [local meteor directory, defaults to .]"
+TEMP_DUMP_LOCATION=/tmp/asteroidmine.mongodump
 # 1. Ensure user has meteor & mongo installed and available in path
 if [ ! $(which meteor) ] ; then
   echo "Meteor is not installed on this computer (or at least it's not in the PATH)"
@@ -23,13 +25,15 @@ if [ ! $(which mongodump) ] ; then
 fi
 
 # 2. Ensure project exists in proper directory
-METEOR_PROJECT_DIR=`pwd`
+ORIGINAL_DIR=$(pwd)
+METEOR_PROJECT_DIR=$(pwd)
 if [ $2 ] ; then
   METEOR_PROJECT_DIR=$2
 fi
 
 if [ ! -d $METEOR_PROJECT_DIR/.meteor ] ; then
   echo "$METEOR_PROJECT_DIR doesn't seem to have a valid Meteor project"
+  echo $USAGE
   exit 1
 fi
 
@@ -39,16 +43,35 @@ echo "(if your deployment has a password, it will be needed here)"
 echo "-----------------------------------------------------------"
 
 METEOR_APP_URL=$1
-MONGO_SERVER_URL=$(meteor mongo --url $METEOR_MONGO_URL)
-#2.  mongodump server into temp
-# sample mongo url: mongodb://client:THIS-IS-PASSWORD@MONGO-SERVER-URL/DATABASE-URL
+cd $METEOR_PROJECT_DIR
+#MONGO_SERVER_URL=$(meteor mongo --url $METEOR_MONGO_URL)
+#MONGO_SERVER_URL="mongodb://client:THIS-IS-PASSWORD@MONGO-SERVER-URL/DATABASE-URL"
+
 # kind of a hacky regex, would love some help.
-MONGO_SERVER_PW=$(echo $MONGO_SERVER_PW | sed "s|mongodb://client:\(.*\)@.*|\1|")
-MONGO_SERVER_HOST=$(echo $MONGO_SERVER_PW | sed "s|.*@\(.*\)/.*|\1|")
-MONGO_SERVER_DBNAME=$(echo $MONGO_SERVER_PW | sed "s|.*/\(.*\)|\1|")
-mongodump --user client --host $MONGO_SERVER_HOST --db $MONGO_SERVER_DBNAME --password $MONGO_SERVER_PW --out /tmp/asteroidmine.mongodump
+MONGO_SERVER_PW=$(echo $MONGO_SERVER_URL | sed "s|mongodb://client:\(.*\)@.*|\1|")
+MONGO_SERVER_HOST=$(echo $MONGO_SERVER_URL| sed "s|.*@\(.*\)/.*|\1|")
+MONGO_SERVER_DBNAME=$(echo $MONGO_SERVER_URL | sed "s|.*/\(.*\)|\1|")
 
+rm -r $TEMP_DUMP_LOCATION
+mongodump --username client --host $MONGO_SERVER_HOST --db $MONGO_SERVER_DBNAME --password $MONGO_SERVER_PW --out $TEMP_DUMP_LOCATION
 
-#4.  mongorestore into local
-echo mongorestore --db meteor -h localhost --port 3002 --drop secondscreen_meteor_com/
-#5.  remove temp
+# TODO check exit code
+echo "-----------------------------------------------------------"
+echo "Uploading DB into your local meteor instance"
+echo "-----------------------------------------------------------"
+
+if [ -n "$(ps ax | grep -e mongod | grep -v grep)" ] ; then
+  echo Mongo already running.
+else
+  # TODO - more than port 3000
+  echo "starting local version of meteor (needed for database daeamon)"
+  meteor &
+fi
+
+mongorestore --db meteor -h localhost --port 3002 --drop $TEMP_DUMP_LOCATION/$MONGO_SERVER_DBNAME/
+
+echo "-----------------------------------------------------------"
+echo "All done, enjoy!"
+echo "-----------------------------------------------------------"
+# TODO ensure exit code is correct
+cd $ORIGINAL_DIR
